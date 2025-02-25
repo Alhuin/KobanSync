e4e66a2aad657f6c7020caa567f094eef4afed41
Alhuin <alhuin.git@proton.me>
2025-02-25 15:04:30 +0100

    English logging

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,28 +3,26 @@
 
 set -e
 
-# Wait until MySQL is ready.
 /usr/local/bin/wait-for-mysql.sh
 
 export WP_CORE_DIR="/var/www/html"
 export WP_TESTS_DIR="/tmp/wordpress-tests-lib"
 PLUGIN_DIR="$WP_CORE_DIR/wp-content/plugins/woocommerce-koban-sync"
 
-# Check if WordPress is already installed.
 if ! wp --allow-root core is-installed --path="$WP_CORE_DIR" >/dev/null 2>&1; then
-  echo "Téléchargement de WordPress..."
+  echo "Downloading WordPress..."
   wp --allow-root core download --force --path="$WP_CORE_DIR"
 
-  echo "Création de la configuration..."
+  echo "Creating configuration files..."
   wp --allow-root config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --skip-check --path="$WP_CORE_DIR"
 
-  echo "Installation de WordPress..."
+  echo "Installing WordPress..."
   wp --allow-root core install --url="http://localhost:8080" --title="TestSite" --admin_user=admin --admin_password=admin --admin_email=admin@example.org --skip-email --path="$WP_CORE_DIR"
 
-  echo "Installation de WooCommerce..."
+  echo "Installing WooCommerce..."
   wp --allow-root plugin install woocommerce --activate --path="$WP_CORE_DIR"
 
-  echo "Création de la suite de tests..."
+  echo "Creating the test suite..."
   mkdir -p /tmp/scaffold-tests
   cd /tmp/scaffold-tests
 
@@ -38,17 +36,16 @@ if ! wp --allow-root core is-installed --path="$WP_CORE_DIR" >/dev/null 2>&1; th
   cd ~
   rm -rf /tmp/scaffold-tests
 
-  echo "Installation de la suite de tests..."
-  # Vider complètement le répertoire de tests pour repartir sur une base propre
+  echo "Installing the test suite..."
   rm -rf "$WP_TESTS_DIR"
   yes | "$PLUGIN_DIR"/bin/install-wp-tests.sh "$WORDPRESS_DB_NAME" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_HOST" 6.7
   rm -rf "${PLUGIN_DIR:?}"/bin/
 
   cd "$PLUGIN_DIR"/tests
-  echo "Installation des dépendances..."
+  echo "Installing dependencies..."
   composer install
 
 fi
 
-echo "Démarrage d'Apache..."
+echo "Running Apache..."
 exec apache2-foreground
